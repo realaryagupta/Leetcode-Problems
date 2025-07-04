76ebdf12678d04abe0954b04e2ed843b2d18b3f0
Arya Gupta <119800001+realaryagupta@users.noreply.github.com>
2025-07-04 13:17:40 +0530

    Time: 5 ms (32.74%) | Memory: 17.8 MB (31.80%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,24 +1,20 @@
 class Solution(object):
     def mySqrt(self, x):
-        """
-        :type x: int
-        :rtype: int
-        """
-
         if x < 2:
             return x
-        
         
         left = 0
         right = x
 
         while left < right:
             mid = left + (right-left) // 2
-
-            if mid * mid <= x:
-                left = mid + 1
+            if mid * mid >= x:
+                right = mid
 
             else:
-                right = mid
+                left = mid + 1
             
-        return left - 1
+        if left * left == x:
+            return left
+        else:
+            return left - 1
