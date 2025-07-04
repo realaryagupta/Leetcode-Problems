1b4bb235289c831578c8af541cfe7ac8ddc8b389
Arya Gupta <119800001+realaryagupta@users.noreply.github.com>
2025-07-04 14:23:11 +0530

    Time: 514 ms (23.35%) | Memory: 17.8 MB (32.33%) - LeetSync

--- 441-arranging-coins/arranging-coins.py
+++ 441-arranging-coins/arranging-coins.py
@@ -2,4 +2,15 @@ import math
 
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        return int((math.sqrt(8 * n + 1) - 1) // 2)
+        count = 0
+
+        for i in range(1, n+1):
+            n = n - i
+
+            if n >= 0:
+                count += 1
+
+            elif n < 0:
+                break
+
+        return count
