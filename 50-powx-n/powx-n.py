de36297757fcc2ebec06b3f01bddcd941ee0ea03
Arya Gupta <119800001+realaryagupta@users.noreply.github.com>
2025-07-15 22:49:49 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (10.84%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,25 +1,13 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        
-        # any number to power 0 is 1
         if n == 0:
-            return 1 
+            return 1
 
-        # if number is negative then make it positive and change power 
         if n < 0:
-            x = 1 / x
-            n = -n
-        
-        ans = 1
+            return 1 / self.myPow(x, -n) 
 
-        while n:
-
-            # is n is odd then we need to multiply ans by the x so that later we can calculate the square of half the power
-            # this is done to reduce the time complexity from n to nlogn because we halved the computaitons
-            if n % 2:
-                ans = (ans * x)
-            
-            x = x * x
-            n = n // 2
-        
-        return ans
+        half = self.myPow(x, n // 2)       
+        if n % 2 == 0:
+            return half * half
+        else:
+            return half * half * x
